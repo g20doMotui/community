742364e95441a4e8e873fb8c844bd52975be3e8b
g20doMotui <shaoxing5776@163.com>
2021-04-07 01:44:53 +0800

    4

---
+++ app/src/main/java/com/community/MemberListActivity.java
@@ -0,0 +1,32 @@
+package com.community;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.easylib.base.BaseActivity;
+
+/**
+ * 成员列表
+ */
+public class MemberListActivity extends BaseActivity {
+
+    @Override
+    public int getContentViewId() {
+        return R.layout.activity_member_list;
+    }
+
+    @Override
+    protected void initView(Bundle savedInstanceState) {
+
+    }
+
+    @Override
+    protected void initVariable() {
+
+    }
+
+    @Override
+    protected void processLogic(Bundle savedInstanceState) {
+
+    }
+}
